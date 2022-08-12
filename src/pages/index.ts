670b62c78a69ad082fc95e11e1b6cdd756fbbef5
MdeMora <miguel.uni.demora@gmail.com>
2022-08-12 10:42:59 +0200

    build error

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,10 +1,12 @@
 import HomeContainer from 'containers/HomeContainer'
 import { fetchData } from 'utils/fetching'
 
+import data from '../constants/data.json'
+
 export default HomeContainer
 
 export async function getStaticProps() {
-  const data = await (await fetchData('api/categories')).json()
+  // const data = await (await fetchData('api/categories')).json()
 
   return {
     props: { ...data }, // will be passed to the page component as props
